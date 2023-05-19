e7ab50c538733fded39cd5b2d9d125f95ee2f84b
Harsh Singh <sujeetsingh734321@gmail.com>
2023-05-19 13:32:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+// Serve the HTML file
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Handle socket connections
+io.on("connection", (socket) => {
+  console.log("A user connected.");
+
+  // Handle joining the room
+  socket.on("join_room", (room) => {
+    // Join the specified room
+    socket.join(room);
+
+    // Handle creating an event
+    socket.on("create_event", (eventData) => {
+      // Broadcast the event data to all connected clients in the room
+      io.to(room).emit("event_created", eventData);
+    });
+  });
+});
+
+// Start the server
+http.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
