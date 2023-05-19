c3c099a8dff6cf8d8e943d1d58586894e2e14a7c
Harsh Singh <sujeetsingh734321@gmail.com>
2023-05-19 15:27:58 +0530

    gdfgfdg

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 // Allow cors
 const cors = require("cors");
