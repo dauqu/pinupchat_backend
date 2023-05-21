dc763d5f246935acca82bfb9e50735c40e39d1db
Harsh Singh <sujeetsingh734321@gmail.com>
2023-05-21 10:22:36 +0530

    adfdsf

--- routes/login.js
+++ routes/login.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
 
   const check = await CheckAuth(req, res);
   if (check.auth === false) {
-    return res.status(401).json({ message: "Unauthorized", auth: false });
+    return res.status(200).json({ message: "Unauthorized", auth: false });
   } else {
     return res.status(200).json({ message: "Authorized", auth: true });
   }
@@ -24,12 +24,12 @@ router.post("/", checkUser, async (req, res) => {
     const user = await UsersSchema.findOne({ email }).lean();
     if (!user)
       return res
-        .status(400)
+        .status(200)
         .json({ message: "User and password is wrong.", status: "warning" });
     const hashpass = user.password;
     if (!bcrypt.compareSync(password, hashpass))
       return res
-        .status(400)
+        .status(200)
         .json({ message: "User and password is wrong.", status: "warning" });
     const token = jwt.sign(
       {
@@ -75,19 +75,19 @@ async function checkUser(req, res, next) {
     password == null
   ) {
     return res
-      .status(400)
+      .status(200)
       .json({ message: "Please fill all fields", status: "warning" });
   }
   //Check email is valid or not
   if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)) {
     return res
-      .status(400)
+      .status(200)
       .json({ message: "Please enter a valid email", status: "warning" });
   }
 
   //Check password is valid or not
   if (req.body.password.length < 6) {
-    return res.status(400).json({
+    return res.status(200).json({
       message: "Password must be at least 6 characters",
       status: "warning",
     });
